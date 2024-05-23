2fd50dbeca7019f588ecd40ad5d110e0bcb56d32
Areesha1416 <143162257+Areesha1416@users.noreply.github.com>
2024-05-24 04:50:03 +0500

    Update index.js

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import AllContext from './context/AllContext';
 import 'react-modal-video/scss/modal-video.scss';
